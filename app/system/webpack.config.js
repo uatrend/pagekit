de49e8fc409cb87ef24407f9331fda0fc2312ff1
Iskender TOTOGLU <totoglu@gmail.com>
2019-08-26 15:34:30 +0300

    update app/system/webpack.config.js

--- app/system/webpack.config.js
+++ app/system/webpack.config.js
@@ -1,4 +1,5 @@
 const assets = `${__dirname}/../assets`;
+const node_modules = `${__dirname}/../../node_modules`;
 
 module.exports = [
 
@@ -11,10 +12,10 @@ module.exports = [
         },
         resolve: {
             alias: {
-                md5$: `${assets}/js-md5/js/md5.js`,
+                md5$: `${node_modules}/js-md5/src/md5.js`,
                 'vue-form$': `${assets}/vue-form/dist/vue-form.common.js`,
                 'vue-intl$': `${assets}/vue-intl/dist/vue-intl.common.js`,
-                'vue-resource$': `${assets}/vue-resource/dist/vue-resource.common.js`,
+                'vue-resource$': `${node_modules}/vue-resource/dist/vue-resource.common.js`,
                 JSONStorage$: `${assets}/JSONStorage/storage.js`,
             },
         },
